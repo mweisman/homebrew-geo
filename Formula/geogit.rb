8f06378bd84db63ed82f909b450f7dce95c330c6
Michael Weisman <mweisman@gmail.com>
2013-04-04 17:34:35 -0700

    Remove maven dependancy

--- Formula/geogit.rb
+++ Formula/geogit.rb
@@ -4,8 +4,6 @@ class Geogit < Formula
   homepage 'https://github.com/opengeo/GeoGit'
   url 'http://localhost:8000/geogit-cli-app-0.1-20130404.zip'
   sha1 '83b7ad9d8585fbe42fd12b7ee732c63e33536c5d'
-  
-  depends_on 'maven'
 
   def install
     rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
