20a1a94d8584225bf2b29168c22a2d6bab63d52a
Michael Weisman <mweisman@gmail.com>
2013-04-05 18:53:26 -0700

    Use stable url

--- Formula/pointcloud.rb
+++ Formula/pointcloud.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Pointcloud < Formula
   homepage 'https://github.com/pramsey/pointcloud'
-  url 'https://github.com/pramsey/pointcloud/archive/master.zip'
+  url 'https://dl.dropbox.com/u/363450/pointcloud-master.zip'
   version '0.1'
   sha1 '675c32c5883a9f1e8dafa77b2cc6c52dcce7e1e4'
   head 'https://github.com/pramsey/pointcloud.git'
